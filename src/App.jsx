f1fc48243a6437a373c2bf945afbb8dca0055f32
nileshpanalink <nilesh@panalinks.com>
2026-04-20 15:02:57 +0530

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import MyPhoto from "../assets/nk.png";
+import MyPhoto from "./assets/nk.png";
 
 const NAV_LINKS = [
   "Home",
